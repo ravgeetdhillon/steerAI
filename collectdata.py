903ebdd27b7e8c1609784516afa5f89d21325b8f
ravgeetdhillon <ravgeetdhillon@gmail.com>
2019-10-07 14:09:38 +0530

    some minor changes

--- collectdata.py
+++ collectdata.py
@@ -1,6 +1,5 @@
 from screen import screenshot, show_image
-from getkeys import key_check
-from keys import keys_to_output
+from keys import keys_to_output, key_check
 import numpy as np
 import cv2
 import time
@@ -39,14 +38,14 @@ def setup():
             break
         else:
             starting_value += 1
-    
+
     return (file_name, part, starting_value)
-        
+
 
 def main():
-    
+
     # gives a countdown before starting the process
-    for i in list( range(2) )[::-1]:
+    for i in list(range(2))[::-1]:
         print(i + 1)
         time.sleep(1)
     print('START')
@@ -61,37 +60,38 @@ def main():
 
     while True:
         if not paused:
-            
+
             # checks the number of images processed per seconds
             count += 1
-            now = time.time() 
+            now = time.time()
             if now - last_time >= 1:
-                print(count)
+                print(f'-FPS {count}')
                 count = 0
                 last_time = now
-            
+
             # grabs the screen, resizes it and then converts in to Grayscale mode
-            screen = screenshot( region=(10, 40, SCREEN_GRAB_WIDTH, SCREEN_GRAB_HEIGHT) )
-            scaled_width = int( SCREEN_GRAB_WIDTH * RESIZE_FACTOR )
-            scaled_height = int( SCREEN_GRAB_HEIGHT * RESIZE_FACTOR )
-            screen = cv2.resize( screen, (scaled_width, scaled_height) )
-            screen = cv2.cvtColor( screen, cv2.COLOR_BGR2GRAY )
+            screen = screenshot(
+                region=(10, 40, SCREEN_GRAB_WIDTH, SCREEN_GRAB_HEIGHT))
+            scaled_width = int(SCREEN_GRAB_WIDTH * RESIZE_FACTOR)
+            scaled_height = int(SCREEN_GRAB_HEIGHT * RESIZE_FACTOR)
+            screen = cv2.resize(screen, (scaled_width, scaled_height))
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
             # # records the key presses for each image
             keys = key_check()
             output = keys_to_output(keys)
 
             # stores the pixel data of the image and the corresponding key presses in the training data array
-            training_data.append( [screen, output] )
+            training_data.append([screen, output])
 
-            # regular checkpoints to keep the track of data collection process            
+            # regular checkpoints to keep the track of data collection process
             if len(training_data) % 100 == 0:
-                print('MILESTONE REACHED')
+                print('--MILESTONE REACHED')
 
             # store the data into the npy file after the dataset size reaches 500 images
             if len(training_data) == SAVE_POINT:
                 np.save(file_name, training_data)
-                print('SAVED')
+                print(f'-----SAVED {starting_value}-----')
                 training_data = []
                 starting_value += 1
                 file_name = f'data/dataset/{part}/training_data-{starting_value}.npy'
